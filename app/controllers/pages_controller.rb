8b3a5409798a1c56824166c8e4d1de9ded6a136e
Marie Leleu <marieleleu@icloud.com>
2021-11-09 15:29:18 +0100

    base of site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def events
+  end
+
+  def infospratique
+  end
+
+  def lejourj
+  end
+
+  def listemariage
+  end
+
+  def rsvp
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope "(:locale)", locale: /en|fr/ do
+    root to: 'pages#home'
+    get 'events', to: 'pages#events'
+    get 'rsvp', to: 'pages#rsvp'
+    get 'infospratique', to: 'pages#infospratique'
+    get 'lejourj', to: 'pages#lejourj'
+    get 'listemariage', to: 'pages#listemariage'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
